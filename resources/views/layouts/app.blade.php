d70ca357902d60c7c2362ef59d237ee31b56e637
Evgenii Poperezhai <eapdob@gmail.com>
2023-08-09 14:20:30 +0300

    - added search dropdown -- added search dropdown livewire -- added tailwind-spinner -- added search dropdown tests

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,8 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
-    @vite(['resources/js/app.js'])
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <livewire:styles>
 </head>
 <body class="font-sans bg-gray-900 text-white">
 <nav class="border-b border-gray-800">
@@ -29,12 +30,7 @@
             </li>
         </ul>
         <div class="flex flex-col md:flex-row items-center">
-            <div class="relative mt-3 md:mt-0">
-                <input type="text" class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
-                <div class="absolute top-0">
-                    <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
-                </div>
-            </div>
+            <livewire:search-dropdown>
             <div class="md:ml-4 mt-3 md:mt-0">
                 <a href="#">
                     <img src="/img/avatar.png" alt="avatar" class="rounded-full w-8 h-8">
@@ -44,5 +40,6 @@
     </div>
 </nav>
 @yield('content')
+<livewire:scripts>
 </body>
 </html>
---
+++ resources/views/livewire/search-dropdown.blade.php
@@ -0,0 +1,44 @@
+<div class="relative mt-3 md:mt-0">
+    <input wire:model.debounce.500ms="search" type="text"
+           class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
+           placeholder="Search">
+    <div class="absolute top-0">
+        <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24">
+            <path class="heroicon-ui"
+                  d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/>
+        </svg>
+    </div>
+
+    <div wire:loading
+         class="spinner top-0 right-0 mr-4 mt-2 m-12 inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+         role="status">
+        <span
+            class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+    </div>
+
+    @if (strlen($search) >= 2)
+        <div class="absolute bg-gray-800 text-sm rounded w-64 mt-4">
+            @if ($searchResults->count() > 0)
+                <ul>
+                    @foreach ($searchResults as $result)
+                        <li class="border-b border-gray-700">
+                            <a href="{{ route('movies.show', $result['id']) }}"
+                               class="block hover:bg-gray-700 px-3 py-3 flex items-center transition ease-in-out duration-150">
+                                @if ($result['poster_path'])
+                                    <img src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path'] }}" alt="poster"
+                                         class="w-8">
+                                @else
+                                    <img src="https://via.placeholder.com/50x75" alt="poster" class="w-8">
+                                @endif
+                                <span class="ml-4">{{ $result['title'] }}</span>
+                            </a>
+                        </li>
+                    @endforeach
+
+                </ul>
+            @else
+                <div class="px-3 py-3">No results for "{{ $search }}"</div>
+            @endif
+        </div>
+    @endif
+</div>
