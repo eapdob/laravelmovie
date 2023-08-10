f2e6a4957ec63bad3e6600625d4a491b70aa1bd2
Evgenii Poperezhai <eapdob@gmail.com>
2023-08-10 12:36:27 +0300

    - added tv page -- added TvController -- added tv, tvshow view model -- added structure of views tv -- added test for tv pages

--- resources/views/tv/show.blade.php
+++ resources/views/tv/show.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="movie-info border-b border-gray-800">
+    <div class="tv-info border-b border-gray-800">
         <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
             <div class="flex-none">
-                <img src="{{ $movie['poster_path'] }}" alt="poster" class="w-64 lg:w-96">
+                <img src="{{ $tvshow['poster_path'] }}" alt="parasite" class="w-64 lg:w-96">
             </div>
             <div class="md:ml-24">
-                <h2 class="text-4xl mt-4 md:mt-0 font-semibold">{{ $movie['title'] }}</h2>
+                <h2 class="text-4xl mt-4 md:mt-0 font-semibold">{{ $tvshow['name'] }}</h2>
                 <div class="flex flex-wrap items-center text-gray-400 text-sm">
                     <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24">
                         <g data-name="Layer 2">
@@ -16,31 +16,28 @@
                                 data-name="star"/>
                         </g>
                     </svg>
-                    <span class="ml-1">{{ $movie['vote_average'] }}</span>
+                    <span class="ml-1">{{ $tvshow['vote_average'] }}</span>
                     <span class="mx-2">|</span>
-                    <span>{{ $movie['release_date'] }}</span>
+                    <span>{{ $tvshow['first_air_date'] }}</span>
                     <span class="mx-2">|</span>
-                    <span>{{ $movie['genres'] }}</span>
+                    <span>{{ $tvshow['genres'] }}</span>
                 </div>
-
                 <p class="text-gray-300 mt-8">
-                    {{ $movie['overview'] }}
+                    {{ $tvshow['overview'] }}
                 </p>
-
                 <div class="mt-12">
-                    <h4 class="text-white font-semibold">Featured Crew</h4>
                     <div class="flex mt-4">
-                        @foreach ($movie['crew'] as $crew)
+                        @foreach ($tvshow['created_by'] as $crew)
                             <div class="mr-8">
                                 <div>{{ $crew['name'] }}</div>
-                                <div class="text-sm text-gray-400">{{ $crew['job'] }}</div>
+                                <div class="text-sm text-gray-400">Creator</div>
                             </div>
+
                         @endforeach
                     </div>
                 </div>
-
                 <div x-data="{ isOpen: false }">
-                    @if (count($movie['videos']['results']) > 0)
+                    @if (count($tvshow['videos']['results']) > 0)
                         <div class="mt-12">
                             <button
                                 @click="isOpen = true"
@@ -73,7 +70,7 @@
                                             <div class="responsive-container overflow-hidden relative"
                                                  style="padding-top: 56.25%">
                                                 <iframe class="responsive-iframe absolute top-0 left-0 w-full h-full"
-                                                        src="https://www.youtube.com/embed/{{ $movie['videos']['results'][0]['key'] }}"
+                                                        src="https://www.youtube.com/embed/{{ $tvshow['videos']['results'][0]['key'] }}"
                                                         style="border:0;" allow="autoplay; encrypted-media"
                                                         allowfullscreen></iframe>
                                             </div>
@@ -86,16 +83,15 @@
                 </div>
             </div>
         </div>
-    </div> <!-- end movie-info -->
-
-    <div class="movie-cast border-b border-gray-800">
+    </div> <!-- end tv-info -->
+    <div class="tv-cast border-b border-gray-800">
         <div class="container mx-auto px-4 py-16">
             <h2 class="text-4xl font-semibold">Cast</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-                @foreach ($movie['cast'] as $cast)
+                @foreach ($tvshow['cast'] as $cast)
                     <div class="mt-8">
                         <a href="{{ route('actors.show', $cast['id']) }}">
-                            <img src="{{ 'https://image.tmdb.org/t/p/w300/'.$cast['profile_path'] }}" alt="actor1"
+                            <img src="{{ $cast['profile_path'] }}" alt="actor1"
                                  class="hover:opacity-75 transition ease-in-out duration-150">
                         </a>
                         <div class="mt-2">
@@ -109,13 +105,12 @@
                 @endforeach
             </div>
         </div>
-    </div> <!-- end movie-cast -->
-
-    <div class="movie-images" x-data="{ isOpen: false, image: ''}">
+    </div> <!-- end tv-cast -->
+    <div class="tv-images" x-data="{ isOpen: false, image: ''}">
         <div class="container mx-auto px-4 py-16">
             <h2 class="text-4xl font-semibold">Images</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-                @foreach ($movie['images'] as $image)
+                @foreach ($tvshow['images'] as $image)
                     <div class="mt-8">
                         <a
                             @click.prevent="
@@ -151,5 +146,5 @@
                 </div>
             </div>
         </div>
-    </div> <!-- end movie-images -->
-@endsection('content')
+    </div> <!-- end tv-images -->
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,7 @@ Route::get('/movies/{movie}', 'App\Http\Controllers\MoviesController@show')->nam
 
 Route::get('/actors', 'App\Http\Controllers\ActorsController@index')->name('actors.index');
 Route::get('/actors/page/{page?}', 'App\Http\Controllers\ActorsController@index');
+Route::get('/actors/{id}', 'App\Http\Controllers\ActorsController@show')->name('actors.show');
 
-Route::get('/actors/{actor}', 'App\Http\Controllers\ActorsController@show')->name('actors.show');
+Route::get('/tv', 'App\Http\Controllers\TvController@index')->name('tv.index');
+Route::get('/tv/{id}', 'App\Http\Controllers\TvController@show')->name('tv.show');
